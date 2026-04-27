613933fe24d1407b621c2c2db255fc60e34c49bf
dgb-asir1 <damasogb58@educastur.es>
2026-04-27 20:47:43 +0200

    script backup con config wireguard

--- Scripts/copia_seguridad_todo.sh
+++ Scripts/copia_seguridad_todo.sh
@@ -6,13 +6,17 @@ LOG="/var/log/backup.log"
 
 echo "$(date "+%Y-%m-%d %H:%M:%S") - Comienzo copia seguridad con BDDD" > "$LOG"
 
+# contenedores - almacenamiento
 rsync -avz --delete /srv/media/ "$DESTINO/media/" >> "$LOG" 2>&1
 rsync -avz --delete /srv/docker/wordpress/ "$DESTINO/wordpress/" >> "$LOG" 2>&1
 rsync -avz --delete /srv/docker/jellyfin/ "$DESTINO/jellyfin/" >> "$LOG" 2>&1
 
-# dump mysql
+# contenedores - dump mysql
 echo "$(date "+%Y-%m-%d %H:%M:%S") - Dump MySQL" >> "$LOG"
 
 docker exec docker-mysql-1 \
   mysqldump -u root -p$(docker exec docker-mysql-1 printenv MYSQL_ROOT_PASSWORD) --all-databases \
   > "$DESTINO/mysql_dump.sql" 2>> "$LOG"
+
+# wireguard
+rsync -avz --delete /etc/wireguard/ "$DESTINO/wireguard/" >> "$LOG" 2>&1
